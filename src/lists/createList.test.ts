ed731d3e0bd5463aabcd29b5d2e2aaa7352f140a
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-10-02 22:23:01 -0300

    test(#98): ✅ Add cases success and error to createList http post
    Introduced success case check to Post Request to be service
    Introduced throw case check EmailOctopusError onPost to createList

--- src/lists/createList.test.ts
+++ src/lists/createList.test.ts
@@ -3,36 +3,43 @@ import AxiosMockAdapter from "axios-mock-adapter";
 import { createList } from "./createList";
 import { List } from "./types";
 import { EmailOctopusError } from "../emailOctopus";
-import { ApiWideErrorResponses } from "../types";
 
 const mockAxios = new AxiosMockAdapter(axios);
 
-describe('CreateList', () => {
+describe('createList', () => {
     const FAKE_API_KEY = "1c2b40da-5fa3-11ee-8c99-0242ac120002";
     const input = { name: 'Lorem ipsun' };
 
-    it('should return success and status 200', async () => {
-        const output: List = {} as List;
-
-        mockAxios.onPost().replyOnce(200, output);
+    it('should successful request', async () => {
+        mockAxios.onPost().replyOnce(200);
         jest.spyOn(axios, "post");
 
-        const result = await createList(FAKE_API_KEY)(input);
+        await createList(FAKE_API_KEY)(input);
 
-        expect(result).toEqual(output);
         expect(axios.post).toBeCalledWith(
             'https://emailoctopus.com/api/1.6/lists',
             { api_key: FAKE_API_KEY, name: 'Lorem ipsun' }
         );
     });
 
-    it('should throw EmailOctopusError', async () => {
+    it('should return a List and status code 201', async () => {
+        const output: List = {} as List;
+
+        mockAxios.onPost().replyOnce(201, output);
+        jest.spyOn(axios, "post");
+
+        const result = await createList(FAKE_API_KEY)(input);
+
+        expect(result).toEqual(output);
+    });
+
+    it('should throw EmailOctopusError to unmaped type error', async () => {
         const output = {
-            code: "INTERNAL_SERVER_ERROR",
+            code: undefined,
             message: "Unexpected Error",
         };
 
-        mockAxios.onPost().replyOnce(404, output);
+        mockAxios.onPost().replyOnce(500, output);
         jest.spyOn(axios, "post");
 
         const result = await createList(FAKE_API_KEY)(input).catch(
@@ -41,4 +48,16 @@ describe('CreateList', () => {
 
         expect(result).toBeInstanceOf(EmailOctopusError);
     });
+
+    it('should throw TypeError to no content response', async () => {
+        mockAxios.onPost().replyOnce(500);
+        jest.spyOn(axios, "post");
+
+        const result = await createList(FAKE_API_KEY)(input).catch(
+            (error) => error,
+        );
+
+        expect(result).toBeInstanceOf(TypeError);
+    });
+
 });
